cb580d6e347dbabb2e2ba753a64c053b6ca2bde5
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2019-02-09 08:24:18 -0200

    Implementando primeira classe de dominio

--- src/main/java/br/com/cenarioesolucao/cursoMC/resources/CategoriaResource.java
+++ src/main/java/br/com/cenarioesolucao/cursoMC/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package br.com.cenarioesolucao.cursoMC.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cenarioesolucao.cursoMC.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 }
